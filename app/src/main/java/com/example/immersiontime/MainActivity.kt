5f0f995263e62e72682b2af04a8a6541e37b3e84
MalekS7 <malek.saleh1994@gmail.com>
2022-01-16 18:54:44 -0500

    Implemented location prodding to the app

--- app/src/main/java/com/example/immersiontime/MainActivity.kt
+++ app/src/main/java/com/example/immersiontime/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.immersiontime
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.media.MediaPlayer
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
     private var mMediaPlayer: MediaPlayer? = null
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvGpsLocation: TextView
+    private val locationPermissionCode = 2
+
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_gps: Button = findViewById(R.id.btn_gps)
+        btn_gps.setOnClickListener{
+            //This should search for and display the locational data
+            getLocation();
+        }
 
         val button: Button = findViewById(R.id.sound_button)
         button.setOnClickListener {
@@ -18,6 +38,37 @@ class MainActivity : AppCompatActivity() {
             soundControl()
         }
     }
+
+    /**
+     * This function should find and display the location data for the phone.
+     */
+    private fun getLocation()
+    {
+        //This gets from the phone's system the context of location service and sets our location manager to it.
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager;
+
+        //Check for permissions first. If permission granted, go a head, if not, ask first.
+        if ((ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED))
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+    override fun onLocationChanged(location: Location) {
+        tvGpsLocation = findViewById(R.id.textView)
+        tvGpsLocation.text = "Latitude: " + location.latitude + " , Longitude: " + location.longitude
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
     private fun soundControl() {
         if (mMediaPlayer == null)
@@ -33,7 +84,7 @@ class MainActivity : AppCompatActivity() {
     private fun playSound() {
         if (mMediaPlayer == null) {
             mMediaPlayer = MediaPlayer.create(this, R.raw.fart)
-            mMediaPlayer!!.isLooping = true
+            mMediaPlayer!!.isLooping = false
             mMediaPlayer!!.start()
         } else mMediaPlayer!!.start()
     }
