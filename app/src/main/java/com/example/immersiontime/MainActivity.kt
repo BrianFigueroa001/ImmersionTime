4c48447ce68a8bf9b50c685448674ab59ee3108e
Brian Figueroa <brianfigueroa001@gmail.com>
2022-01-16 17:29:09 -0500

    initialize repository

---
+++ app/src/main/java/com/example/immersiontime/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.immersiontime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.media.MediaPlayer
+
+var mMediaPlayer: MediaPlayer? = null
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.sound_button)
+        button.setOnClickListener {
+            // Code here executes on main thread after user presses button
+            soundControl()
+        }
+    }
+
+    private fun soundControl() {
+        if (mMediaPlayer == null)
+        {
+            playSound()
+        }
+        else {
+            stopSound()
+        }
+    }
+
+    // 1. Plays the water sound
+    private fun playSound() {
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.fart)
+            mMediaPlayer!!.isLooping = true
+            mMediaPlayer!!.start()
+        } else mMediaPlayer!!.start()
+    }
+
+//    // 2. Pause playback
+//    fun pauseSound() {
+//        if (mMediaPlayer != null && mMediaPlayer!!.isPlaying) mMediaPlayer!!.pause()
+//    }
+
+    // 3. {optional} Stops playback
+    private fun stopSound() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer!!.stop()
+            mMediaPlayer!!.release()
+            mMediaPlayer = null
+        }
+    }
+
+    // 4. Closes the MediaPlayer when the app is closed
+    override fun onStop() {
+        super.onStop()
+        if (mMediaPlayer != null) {
+            mMediaPlayer!!.release()
+            mMediaPlayer = null
+        }
+    }
+}
