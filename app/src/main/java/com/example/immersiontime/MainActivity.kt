7b62e87941102bfb362f7f8500979d5ed30fac5d
Brian Figueroa <brianfigueroa001@gmail.com>
2022-01-16 19:01:48 -0500

    Added GPS tracking and sound playing functionality

--- app/src/main/java/com/example/immersiontime/MainActivity.kt
+++ app/src/main/java/com/example/immersiontime/MainActivity.kt
@@ -1,21 +1,63 @@
 package com.example.immersiontime
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.media.MediaPlayer
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
     private var mMediaPlayer: MediaPlayer? = null
-    
+    private lateinit var locationManager: LocationManager
+    private var locationPermissionCode = 2
+    private var locationUpdateCounter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getLocation()
 
         val button: Button = findViewById(R.id.sound_button)
         button.setOnClickListener {
             // Code here executes on main thread after user presses button
             soundControl()
+        }
+    }
+
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationPermissionCode)
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        println("\n-----------------------------\n")
+        println("Count: " + ++locationUpdateCounter + " Longitude: " + location.latitude + " Longitude: " + location.longitude)
+        println("\n---------------------------\n")
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == locationPermissionCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -55,9 +97,6 @@ class MainActivity : AppCompatActivity() {
     // 4. Closes the MediaPlayer when the app is closed
     override fun onStop() {
         super.onStop()
-        if (mMediaPlayer != null) {
-            mMediaPlayer!!.release()
-            mMediaPlayer = null
-        }
+        stopSound()
     }
 }
